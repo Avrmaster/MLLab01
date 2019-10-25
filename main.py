4257899ebaeb770def5fba373ac6ee4e548966b6
Oleksandr Leskiv <oleksandr.leskiv@digia.co>
2019-10-25 22:13:34 +0300

    min_max normalization

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import numpy as np
 from typing import Tuple
+from sklearn import preprocessing
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 
 def separate_prices(normal_data: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     x_arr = normal_data[:, list(range(1, len(data[0])))]
-    y_arr = normal_data[:, 0]
+    y_arr = normal_data[:, [0]]
 
     ones = np.ones([x_arr.shape[0], 1])
     return np.concatenate((ones, x_arr), axis=1), y_arr
@@ -22,33 +23,34 @@ with open('./dataset/prices_processed.csv') as file:
     # type: np.array
     data = np.array([[float(v) for v in l.split(',')] for l in file.readlines()[1:]])
 
-    stds = np.repeat(np.array([[np.std(d) for d in data.T]]), len(data), axis=0)
-    means = np.repeat(np.array([[np.mean(d) for d in data.T]]), len(data), axis=0)
+    mins = np.repeat(np.array([[np.min(d) for d in data.T]]), len(data), axis=0)
+    maxs = np.repeat(np.array([[np.max(d) for d in data.T]]), len(data), axis=0)
 
-    normal_data = (data - means) / stds
-
-    for i in range(10):
-        print([int(d) for d in data[i]])
-        print(normal_data[i])
-        print()
+    normal_data = (data - mins) / (maxs - mins)
 
     inputs, prices = separate_prices(normal_data[:20000])
     test_inputs, test_prices = separate_prices(normal_data[20000:])
 
+    for i in range(20):
+        print([int(d) for d in data[i]])
+        print(inputs[i])
+        print()
+
     trained_model = train(inputs, prices)
     predicted_prices = trained_model.predict(test_inputs)
 
-    unnormalized_train_prices = (test_prices * stds[0][0]) + means[0][0]
-    unnormalized_predictions = (predicted_prices * stds[0][0]) + means[0][0]
+    unnormalized_train_prices = (test_prices * (maxs - mins)[0][0]) + mins[0][0]
+    unnormalized_predictions = (predicted_prices * (maxs - mins)[0][0]) + mins[0][0]
 
-    # for it in range(10):
+    # for it in range(50):
     #     i = len(predicted_prices) - it - 1
     #
     #     # print(test_inputs[i], ' - ', test_prices[i], ' - ', predicted_prices[i])
+    #     # print(test_prices[i], ' - ', predicted_prices[i])
     #     print(int(unnormalized_train_prices[i]), ' - ', int(unnormalized_predictions[i]))
 
     # print(trained_model.coef_)
-    # print(mean_squared_error(test_prices, predicted_prices))
+    # print(mean_squared_error(unnormalized_train_prices, unnormalized_predictions))
 
     # for i in range(len(test_prices)):
     #     print(abs(test_prices[i] - predicted[i]))
