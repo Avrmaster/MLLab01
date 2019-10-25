1e82a6d3a5f15e95f728122ecd8fca4e9aa296b3
Oleksandr Leskiv <oleksandr.leskiv@digia.co>
2019-10-25 19:26:36 +0300

    data preprocessing performed

---
+++ dataset/data_preprocessor.py
@@ -0,0 +1,18 @@
+with open('./house_prices_data_filtered.csv') as data,\
+        open('./house_prices_data_processed.csv', 'w') as out:
+    header = True
+    for l in data:
+        if header:
+            original_headers = l.strip().split(';')
+            new_headers = original_headers[:12] + ['age', 'was_rebuilt'] + original_headers[14:]
+            out.write(','.join(new_headers)+'\n')
+            header = False
+            continue
+
+        row_data = l.strip().split(';')
+
+        year = int(row_data[12])
+        rec = int(row_data[13])
+
+        new_data = row_data[:12] + [str(2019 - max(year, rec)), str(1 if rec > 0 else 0)] + row_data[14:]
+        out.write(','.join(new_data) + '\n')
